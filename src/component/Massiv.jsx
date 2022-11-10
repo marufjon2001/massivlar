f7fe5ee0804dbc3b63d9a09d1fa38626249f630e
Maruf <marufsayfiyev483@gmail.com>
2022-11-10 21:18:47 +0500

    react massiv

--- src/component/Massiv.jsx
+++ src/component/Massiv.jsx
@@ -1,94 +1,104 @@
 import React, { Component } from "react";
+
 export default class Massiv extends Component {
   state = {
-    user: [
-      {
-        id_: Math.trunc(Math.random() * 999),
-        name: "maruf",
-        age: 21,
-        lastname: "Sayfiyev",
-        region: "Samarqand",
-        district: "ishtixon",
-      },
-      {
-        id_: Math.trunc(Math.random() * 999),
-        name: "Dilshod",
-        age: 22,
-        lastname: "Murtozoyev",
-        region: "Buxoro",
-        district: "Kogon",
-      },
-      {
-        id_: Math.trunc(Math.random() * 999),
-        name: "maruf",
-        age: 21,
-        lastname: "Sayfiyev",
-        region: "Samarqand",
-        district: "ishtixon",
-      },
-      {
-        id_: Math.trunc(Math.random() * 999),
-        name: "Dilshod",
-        age: 22,
-        lastname: "Murtozoyev",
-        region: "Buxoro",
-        district: "Kogon",
-      },
-      {
-        id_: Math.trunc(Math.random() * 999),
-        name: "maruf",
-        age: 21,
-        lastname: "Sayfiyev",
-        region: "Samarqand",
-        district: "ishtixon",
-      },
-      {
-        id_: Math.trunc(Math.random() * 999),
-        name: "Dilshod",
-        age: 22,
-        lastname: "Murtozoyev",
-        region: "Buxoro",
-        district: "Kogon",
-      },
-    ],
-  };
-  deletebtn = (i)=>{
-    let {user} = this.state
-   user.splice(i, 1)
+    valin: '',
+    last: '',
+    age1: '',
+    email1: '',
+   data: [
+    {
+      name: '',
+      LastName: '',
+      Age: '',
+      Email: '',
+    }
+   ]
+  }
+  nameinput = (i)=>{
     this.setState({
-      user
+      valin:i.target.value,
+    })
+  };
+  lastinput = (a)=>{
+    this.setState({
+      last:a.target.value,
     })
   }
+  ageinput = (i)=>{
+    this.setState({
+      age1:i.target.value,
+    })
+  }
+  emailinput = (i)=>{
+    this.setState({
+email1:i.target.value
+    })
+  }
+
+  Addbtn = ()=>{
+    let {data,valin,last,age1,email1} = this.state
+    data.push({
+      name: '',
+      LastName: '',
+      Age: '',
+      Email: '',
+    })
+    this.setState({
+      name:valin,
+      LastName:last,
+      Age:age1,
+      Email:email1
+
+    })
+  };
+
+
   render() {
     return (
-      <div>
-        <table border={"1"} width="100%">
+      <div className="container">
+        <button style={{width: '100px', borderRadius: '10px', height: '20px',marginLeft: '20px', marginTop: '20px'}} onClick ={this.Adduser}>add user</button>
+        <button style={{width: '100px', borderRadius: '10px', height: '20px'}}>close</button>
+        <div className="navbar" style={{display:'flex', width: '50%',justifyContent:'space-between',marginLeft: '20%',marginTop: '5%',}}>
+        <div className="top">
+          <h4>Name</h4>
+          <input type="text" onChange={this.nameinput}/>
+          <h4>Last Name</h4>
+          <input type="text" onChange={this.lastinput}/>
+        </div>
+          <div><input type="submit" style={{width: '100px', height: '30px',borderRadius: '15px', marginTop: '20px',marginTop: '150px', background: 'green'}} onClick ={this.Addbtn}/></div>
+        <div className="bottom">
+        <h4>Age</h4>
+          <input type="text" onChange={this.ageinput}/>
+          <h4>Email</h4>
+          <input type="text" onChange={this.emailinput}/>
+        </div>
+        </div>
+        <table border={1} style={{width:'60%', border: '2',marginLeft: '18%',marginTop: '5%' }}>
           <thead>
             <tr>
-              <th>N</th>
-              <th>id_</th>
               <th>Name</th>
+              <th>LastNAme</th>
               <th>Age</th>
-              <th>Lastname</th>
-              <th>Region</th>
-              <th>District</th>
-              <th colSpan={2}></th>
+              <th>Email</th>
             </tr>
+           {
+            this.state.data. lenght !== 0 ? this.state.data.map((item, index)=>(
+              <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.LastName}</td>
+              <td>{item.Age}</td>
+              <td>{item.Email}</td>
+            </tr>
+            )): ''}
           </thead>
           <tbody>
-            {this.state.user.map((item, index) => 
-              <tr key={index}>
-                <td>{index +1}</td>
-                <td>{item.id_}</td>
-                <td>{item.name}</td>
-                <td>{item.age}</td>
-                <td>{item.lastname}</td>
-                <td>{item.region}</td>
-                <td>{item.district}</td>
-                <td><button>submit</button></td>
-                <td><button onClick={()=>this.deletebtn(index)}>delete</button></td>
-              </tr>
-            )}
+            <tr>
+              <td>{this.state.name}</td>
+              <td>{this.state.LastName}</td>
+              <td>{this.state.Age}</td>
+              <td>{this.state.Email}</td>
+            </tr>
           </tbody>
         </table>
       </div>
